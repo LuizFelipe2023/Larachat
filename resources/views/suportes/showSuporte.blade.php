a1e1b9fd1b0f08daa3f680ea06d8008dc6489f5a
LuizFelipe2023 <luizfelipefroisneves@gmail.com>
2025-02-21 08:31:02 -0400

    algumas melhorias visuais no show das mensagens de suporte.

--- resources/views/suportes/showSuporte.blade.php
+++ resources/views/suportes/showSuporte.blade.php
@@ -5,6 +5,7 @@
 <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
 <script defer src="{{ asset('ckeditor/ckeditor.js') }}"></script>
 <link rel="stylesheet" href="{{ asset('css/createSuporte.css') }}">
+<link rel="stylesheet" href="{{ asset('css/suporteForm.css') }}">
 <script defer>
     document.addEventListener("DOMContentLoaded", function() {
         let ckeditorConfig = {
@@ -20,7 +21,6 @@
         if (document.getElementById('descricao')) {
             CKEDITOR.replace('descricao', ckeditorConfig);
         }
-
     });
 </script>
 @section('content')
@@ -35,96 +35,94 @@
             @foreach ($suportes as $suporte)
             <div class="card rounded-2 shadow-sm border-1 mb-3">
                 <div class="card-header">
-                    <h5 class="fw-bold mt-2 mb-2">Suporte {{ $suporte->id }} do Cliente {{ $suporte->nome_cliente }}
-                    </h5>
+                    <h5 class="fw-bold mt-2 mb-2">Mensagem #{{ $suporte->id }} do {{ $suporte->nome_cliente }}</h5>
                 </div>
                 <div class="card-body">
                     <div class="row g-3">
-                    @if ($errors->any())
+                        @if ($errors->any())
                         <div class="alert alert-danger">
                             <ul>
                                 @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
+                                <li>{{ $error }}</li>
                                 @endforeach
                             </ul>
                         </div>
-                    @endif
-                    @if (session('success'))
+                        @endif
+                        @if (session('success'))
                         <div class="alert alert-success">
                             {{ session('success') }}
                         </div>
-                    @endif
-
-                    @if (session('error'))
+                        @endif
+                        @if (session('error'))
                         <div class="alert alert-danger">
                             {{ session('error') }}
                         </div>
-                    @endif
-                    <div class="col-md-6 mb-3">
-                        <label class="fw-bold">Nome:</label>
-                        <input type="text" value="{{ $suporte->nome_cliente }}" class="form-control" disabled>
-                    </div>
-                    <div class="col-md-6 mb-3">
-                        <label class="fw-bold">Telefone:</label>
-                        <input type="text" value="{{ $suporte->telefone_cliente }}" class="form-control" disabled>
-                    </div>
-                    <div class="col-md-6 mb-3">
-                        <label class="fw-bold">CPF:</label>
-                        <input type="text" value="{{ $suporte->cpf }}" class="form-control" disabled>
-                    </div>
-                    <div class="col-md-6 mb-3">
-                        <label class="fw-bold">Email:</label>
-                        <input type="email" value="{{ $suporte->email_cliente }}" class="form-control" disabled>
-                    </div>
-                    <div class="col-md-12 mb-3">
-                        <label class="fw-bold">Tipo de Dúvida:</label>
-                        <input type="text" value="{{ $suporte->tipo_duvida }}" class="form-control" disabled>
-                    </div>
-                    <div class="col-12 mb-3">
-                        <label class="fw-bold">Descrição:</label>
-                        <textarea class="form-control" id="descricao" rows="5" disabled>{{ $suporte->descricao }}</textarea>
-                    </div>
-                    <div class="d-flex justify-content-end gap-2">
-                        <button type="button" class="btn btn-md btn-secondary" data-bs-toggle="modal" data-bs-target="#inserirAvaliacao{{ $suporte->id }}">
-                            Avaliar
-                        </button>
-                        <a href="{{ route('home') }}" class="btn btn-md btn-warning">Voltar para home</a>
-                    </div>
-                    <div class="modal fade" id="inserirAvaliacao{{ $suporte->id }}" tabindex="-1" aria-labelledby="modalLabel{{ $suporte->id }}" aria-hidden="true">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="modalLabel{{ $suporte->id }}">Inserir Avaliação</h5>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Fechar"></button>
-                                </div>
-                                <form action="{{ route('suportes.inserirAvaliacao', $suporte->id) }}" method="POST">
-                                    @csrf
-                                    @method('PUT')
-                                    <div class="modal-body">
-                                        <div class="mb-3">
-                                            <label for="avaliacao_id" class="form-label">Selecione a Avaliação:</label>
-                                            <select class="form-control" id="avaliacao_id" name="avaliacao_id" required>
-                                                <option value="" disabled selected>Escolha uma opção</option>
-                                                @foreach ($tiposAvaliacoes as $tipoAvaliacao )
-                                                <option value="{{ $tipoAvaliacao->id }}">{{ $tipoAvaliacao->nome }}</option>
-                                                @endforeach
-                                            </select>
+                        @endif
+                        <div class="col-md-6 mb-3">
+                            <label class="fw-bold">Nome:</label>
+                            <input type="text" value="{{ $suporte->nome_cliente }}" class="form-control" disabled>
+                        </div>
+                        <div class="col-md-6 mb-3">
+                            <label class="fw-bold">Telefone:</label>
+                            <input type="text" value="{{ $suporte->telefone_cliente }}" class="form-control" disabled>
+                        </div>
+                        <div class="col-md-6 mb-3">
+                            <label class="fw-bold">CPF:</label>
+                            <input type="text" value="{{ $suporte->cpf }}" class="form-control" disabled>
+                        </div>
+                        <div class="col-md-6 mb-3">
+                            <label class="fw-bold">Email:</label>
+                            <input type="email" value="{{ $suporte->email_cliente }}" class="form-control" disabled>
+                        </div>
+                        <div class="col-md-12 mb-3">
+                            <label class="fw-bold">Tipo de Dúvida:</label>
+                            <input type="text" value="{{ $suporte->tipo_duvida }}" class="form-control" disabled>
+                        </div>
+                        <div class="col-12 mb-3">
+                            <label class="fw-bold">Descrição:</label>
+                            <textarea class="form-control" id="descricao" rows="5" disabled>{{ $suporte->descricao }}</textarea>
+                        </div>
+                        <div class="d-flex justify-content-end gap-2">
+                            <button type="button" class="btn btn-md btn-secondary" data-bs-toggle="modal" data-bs-target="#inserirAvaliacao{{ $suporte->id }}">
+                                Avaliar
+                            </button>
+                            <a href="{{ route('home') }}" class="btn btn-md btn-warning">Voltar para home</a>
+                        </div>
+                        <div class="modal fade" id="inserirAvaliacao{{ $suporte->id }}" tabindex="-1" aria-labelledby="modalLabel{{ $suporte->id }}" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="modalLabel{{ $suporte->id }}">Inserir Avaliação</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Fechar"></button>
+                                    </div>
+                                    <form action="{{ route('suportes.inserirAvaliacao', $suporte->id) }}" method="POST">
+                                        @csrf
+                                        @method('PUT')
+                                        <div class="modal-body">
+                                            <div class="mb-3">
+                                                <label for="avaliacao_id" class="form-label">Selecione a Avaliação:</label>
+                                                <select class="form-control" id="avaliacao_id" name="avaliacao_id" required>
+                                                    <option value="" disabled selected>Escolha uma opção</option>
+                                                    @foreach ($tiposAvaliacoes as $tipoAvaliacao)
+                                                    <option value="{{ $tipoAvaliacao->id }}">{{ $tipoAvaliacao->nome }}</option>
+                                                    @endforeach
+                                                </select>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div class="modal-footer">
-                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
-                                        <button type="submit" class="btn btn-primary">Salvar Avaliação</button>
-                                    </div>
-                                </form>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                                            <button type="submit" class="btn btn-primary">Salvar Avaliação</button>
+                                        </div>
+                                    </form>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-            </div>
+                    </div> 
+                </div> 
+            </div> 
+            @endforeach
+            @endif
         </div>
-        @endforeach
-        @endif
     </div>
-</div>
 </div>
 @endsection
